debc47506c19ba340ff777dca546df2a1409d6de
Juan-Salave <juansalave@gmail.com>
2024-06-17 18:20:41 -0300

    Se agrego map en la pagina principal

--- departamentos/views.py
+++ departamentos/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import Departamento
 from django.views.generic.detail import DetailView
+import folium
+
+# def home(request):
+#     departamentos = Departamento.objects.all()
+#     data = {
+#         'departamentos':departamentos
+#     }
+#     return render(request, 'departamentos/home.html', data)
 
 def home(request):
+    # get departamentos
     departamentos = Departamento.objects.all()
+    # Get ubicacion
+    initialMap = folium.Map(location=[-34.594375,-58.4458285], zoom_start = 12) 
+    # Context con multiples parametros
     data = {
+        'map':initialMap._repr_html_(),
         'departamentos':departamentos
-    }
+        }         
     return render(request, 'departamentos/home.html', data)
 
 class DepartamentoDetailView(DetailView):
